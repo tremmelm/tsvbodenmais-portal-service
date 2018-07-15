1a068168eeb9a67e8e9adc463de54e3f5e01eb24
tremmelm <markus.tremmel@freenet.de>
2018-07-15 22:14:38 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# TsvBodenmais Portal Service
+
+
+## Test
+
+getRoster: http://localhost:8081/getRoster --> Authorization: Bearer e....
+login: http://localhost:8081/login --> 
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'username': 'tsv1905',
-  'password': 'tsvBodenmais1905',
-  'secret': 'Tsv1905AKlasseRegen2018/2019'
+  'username': 'TsvBodenmais',
+  'password': 'TsvBodenmais',
+  'secret': 'TsvBodenmais'
 };
