23b528343dd77c731ebb8809e2884c8c7fa965c9
tremmelm <markus.tremmel@freenet.de>
2018-07-28 22:38:33 +0200

    save roster to filesystem

--- index.js
+++ index.js
@@ -8,75 +8,85 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/login', function (req, res) {
-	
-   //var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-   let user = {
-    username : req.body.username,
-    password : req.body.password
-  };
-  
-  if(user.username === config.username && user.password === config.password){
-	  	  
-	const payload = {
-      id: user.username
+app.post('/login', function(req, res) {
+
+    //var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    let user = {
+        username: req.body.username,
+        password: req.body.password
     };
-	var token = jwt.sign(payload, config.secret, {
-      expiresIn: 86400
-    });
-    res.status(200).send({ auth: true, token: token });
-  }
-  else{
-	res.status(401).json({ message: 'Authentication failed.' });  
-  }
-   
+
+    if (user.username === config.username && user.password === config.password) {
+
+        const payload = {
+            id: user.username
+        };
+        var token = jwt.sign(payload, config.secret, {
+            expiresIn: 86400
+        });
+        res.status(200).send({ auth: true, token: token });
+    } else {
+        res.status(401).json({ message: 'Authentication failed.' });
+    }
+
 })
 
 app.get('/getRoster', (req, res) => {
-   var readRoster = () => {fs.readFile('roster.json', (err, data) => {
-					res.writeHead(200, {'Content-Type': 'application/json'});
-					res.write(data);
-					res.end();
-   })};
-	
-	handleAuthtentication(req, res, readRoster);
+    var readRoster = () => {
+        fs.readFile('roster.json', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(data);
+            res.end();
+        })
+    };
+
+    handleAuthtentication(req, res, readRoster);
 })
 
 app.post('/updateRoster', (req, res) => {
-	
+    var saveRoster = () => {
+        console.log(req.body);
+        fs.writeFile("roster.json", JSON.stringify(req.body), function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("roster saved");
+        });
+    };
+
+    handleAuthtentication(req, res, saveRoster);
 })
 
-function handleAuthtentication(req, res, callback){
-	var token = req.headers['authorization'];
-	console.log(token);
-	if (!token) {
-		res.status(401).send({ auth: false, message: 'No token provided.' });
-	}
-	
-	token = token.replace('Bearer ', '');
+function handleAuthtentication(req, res, callback) {
+    var token = req.headers['authorization'];
+    console.log(token);
+    if (!token) {
+        res.status(401).send({ auth: false, message: 'No token provided.' });
+    }
 
-	jwt.verify(token, config.secret, function(err, decoded) {		
-		if (err) {
-			res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-		}
-		else{
-			if(decoded.id){
-				 callback();
-			}
-			else{
-				res.status(500).send({ auth: false, message: 'Failed to authenticate token.' }); //TODO: change to internal Error 400
-			}
-		}
-	});
+    token = token.replace('Bearer', '').trim();
+    console.log(token);
+
+    jwt.verify(token, config.secret, function(err, decoded) {
+        if (err) {
+            res.status(500).send({ auth: false, message: 'Failed to authenticate token.' + err });
+        } else {
+            if (decoded.id) {
+                callback();
+            } else {
+                res.status(500).send({ auth: false, message: 'Failed to authenticate token.' }); //TODO: change to internal Error 400
+            }
+        }
+    });
 }
 
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+var server = app.listen(8081, function() {
+    var host = server.address().address
+    var port = server.address().port
 
+    console.log("Example app listening at http://%s:%s", host, port)
+})
